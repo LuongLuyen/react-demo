2ba3e464413869849871bd6dd2d36faed6e8874a
LuongLuyen <luongvanluyen2003@gmail.com>
2024-01-20 21:56:12 +0700

    basic dev

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState()
+  const [server, setServer] = useState([])
+  console.log(server[0])
+  
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const res = await axios.get("https://server-97i3.onrender.com/api/posts/film")
+      setServer(res.data)
+    }
+    fetchData()
+  },[])
+  const sendData = async()=>{
+    try {
+      await axios.post("",{dataClient: data})
+      setData('')
+    } catch (error) {
+      return null
+    }
+  }
+  const handleInput = (e)=>{
+    setData(e.target.value)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nginx react
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div>server</div>
+          {server.map((x)=>(
+            <div key={x._id}>
+             <img src ={x.img} alt='img'/>
+             <div>{x.name}</div>
+            </div>
+          ))}
+        </div>
+        <div>Client</div>
+        <input value={data} onChange={handleInput}/>
+        <div onClick={sendData}>sendData</div>
       </header>
     </div>
   );
